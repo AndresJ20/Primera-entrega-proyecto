c92f5ba4b1df781170f42a0b598fad521b62a5ed
AndresJ20 <99735481+AndresJ20@users.noreply.github.com>
2022-02-24 19:20:40 -0500

    Create Módulo.py

---
+++ Operadores básicos/Módulo.py
@@ -0,0 +1,10 @@
+Modulo = 10 % 9
+print ("El módulo es: ",Modulo)
+
+a = 9 ; b = 8
+print("El módulo es: ",a%b)
+
+n1 = int(input("Registe un número: "))
+n2 = int(input("Registe un número: "))
+operacion = n1 % n2
+print("El módulo es: ", operacion)
